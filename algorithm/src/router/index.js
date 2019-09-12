183ff7ffcc377d414be46eacb307dde44e8fb3c8
李吴奇 <liwuqi@liwuqideMacBook-Pro.local>
2019-09-12 10:30:00 +0800

    开始分享

--- algorithm/src/router/index.js
+++ algorithm/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import maze from '@/components/maze'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'maze',
+      component: maze
     }
   ]
 })
